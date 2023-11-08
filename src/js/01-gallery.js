4c6ac535697dc41f9eafb1bc54195a2ee0ce7afa
bestlucky123 <projonurs1216@gmail.com>
2023-11-08 19:30:13 +0100

    Added `SimpleLightbox` npm dependency

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
+// Described in documentation;
+import SimpleLightbox from 'simplelightbox';
+
+// Additional styles import
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Create Gallery
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryImage = galleryItems.map(element => {
+  const galleryItem = document.createElement('div');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('galery__link');
+  galleryLink.href = element.original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = element.preview;
+  image.alt = element.description;
+
+  galleryLink.appendChild(image);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+});
+
+galleryContainer.append(...galleryImage);
+
+// Initialization of SimpleLightbox
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSpeed: 100,
+  fadeSpeed: 150,
+});
