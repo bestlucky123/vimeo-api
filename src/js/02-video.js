08e5abfa05509daaf475ad6f9fba7bde1f42f650
bestlucky123 <projonurs1216@gmail.com>
2023-11-14 18:12:17 +0100

    Initialized the player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+// Added the @vimeo/player library as a project dependency via npm
 
+// Import Vimeo Player
+import Player from '@vimeo/player';
+
+// Find the iframe element on page
+const iframe = document.querySelector('iframe');
+
+// Create an instance of Vimeo Player
+const player = new Player(iframe);
+
+// Set an event handler for the 'play' event
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+// Call the getVideoTitle method after initializing the player
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
