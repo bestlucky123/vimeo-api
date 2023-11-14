c055b7de8c1887a09d7838efa0bc676a0295f90c
bestlucky123 <projonurs1216@gmail.com>
2023-11-14 18:36:34 +0100

    Saved playback time to `localStorage`

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
 // Added the @vimeo/player library as a project dependency via npm
 
-// Import Vimeo Player
+// Import Vimeo Player and throttle
 import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
 // Find the iframe element on page
 const iframe = document.querySelector('iframe');
@@ -9,12 +10,37 @@ const iframe = document.querySelector('iframe');
 // Create an instance of Vimeo Player
 const player = new Player(iframe);
 
-// Set an event handler for the 'play' event
-player.on('play', function () {
-  console.log('played the video!');
-});
+// Define the key for local storage
+const localStorageKey = 'videoplayer-current-time';
 
-// Call the getVideoTitle method after initializing the player
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// Define the callback function for the timeupdate event
+const onTimeUpdate = function (data) {
+  // Save the current time to local storage
+  localStorage.setItem(localStorageKey, data.seconds);
+  //   console.log('Current time saved to localStorage:', data.seconds);
+};
+
+// Add a throttled listener for the timeupdate event, limiting onTimeUpdate calls to every 1000 milliseconds.
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+// ---------------------------------------- TESTS ----------------------------------------
+
+// // Set an event handler for the 'play' event
+// player.on('play', function () {
+//   console.log('Video started playing.');
+// });
+
+// // Call the getVideoTitle method after initializing the player
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+// // Definition of a callback function for the timeupdate event
+// const onTimeUpdate = function (data) {
+//   // data - is an object containing information about the current playback time
+//   console.log('Current time:', data.seconds, 'of', data.duration, 'seconds');
+//   console.log('Percentage played:', data.percent * 100, '%');
+// };
+
+// // Set the listener of the 'timeupdate' event and specify the callback function onTimeUpdate
+// player.on('timeupdate', onTimeUpdate);
